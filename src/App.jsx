532ed3f954832a1bb6396246ada2847824ccfdbd
Jossy Benitez <jossybr94@gmail.com>
2023-10-13 22:17:30 -0500

    Creacion de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
-import "./App.css";
+// import "./App.css";
+// import Home from "./components/Home";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      
+      <div>
+        Aqui estoy 
+      </div>
     </>
   );
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div>nav</div>
+      <div>Inicio</div>
+      <div>Acerca de mi</div>
+      <div>Proyectos</div>
+      <div>Habilidades</div>
+      <div>Footer</div>
+    </div>
+  );
+};
