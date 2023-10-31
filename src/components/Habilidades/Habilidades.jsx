4c3ca36802ccdd84e576ecd435283ae0b4a01abc
Jossy Benitez <jossybr94@gmail.com>
2023-10-30 19:35:52 -0500

    Creando archivo allHabilidades.js

--- src/components/Habilidades/Habilidades.jsx
+++ src/components/Habilidades/Habilidades.jsx
@@ -1,40 +1,14 @@
 import React from "react";
 import Prueba from "../../assets/Foto2.jpg";
 import { css, html, react, node, redux, figma } from "../../assets";
-
-const habilidades = [
-  {
-    imagen: css,
-    texto: " CSS",
-  },
-  {
-    imagen: html,
-    texto: "HTML",
-  },
-  {
-    imagen: react,
-    texto: "React",
-  },
-  {
-    imagen: node,
-    texto: "Node JS",
-  },
-  {
-    imagen: redux,
-    texto: "Redux",
-  },
-  {
-    imagen: figma,
-    texto: "Figma",
-  },
-];
+import allHabilidades from "./allHabilidades";
 
 const Habilidades = () => {
   return (
     <div>
       <h2 className="text-6xl font-bold mb-5">Habilidades</h2>
       <div className="flex flex-wrap justify-center  p-11 gap-44">
-        {habilidades.map((habilidad, index) => (
+        {allHabilidades.map((habilidad, index) => (
           <div
             key={index}
             className="flex flex-col w-1/5 items-center font-bold"
---
+++ src/components/Habilidades/allHabilidades.js
@@ -0,0 +1,30 @@
+import { css, html, react, node, redux, figma } from "../../assets";
+
+const allHabilidades = [
+  {
+    imagen: css,
+    texto: " CSS",
+  },
+  {
+    imagen: html,
+    texto: "HTML",
+  },
+  {
+    imagen: react,
+    texto: "React",
+  },
+  {
+    imagen: node,
+    texto: "Node JS",
+  },
+  {
+    imagen: redux,
+    texto: "Redux",
+  },
+  {
+    imagen: figma,
+    texto: "Figma",
+  },
+];
+
+export default allHabilidades;
