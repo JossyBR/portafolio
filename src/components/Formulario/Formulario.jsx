8bee3686d70422d48045aa5cad2881d071fe5c9f
Jossy Benitez <jossybr94@gmail.com>
2023-11-16 22:16:58 -0500

    Traabajando validaciones

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -1,127 +1,19 @@
-// import React, { useRef } from "react";
-// import emailjs from "@emailjs/browser";
-// import { Input, Button, Textarea } from "@material-tailwind/react";
-// import validaciones from "./validaciones";
-
-// export const Formulario = () => {
-//   const form = useRef();
-
-//   const sendEmail = (e) => {
-//     e.preventDefault();
-
-//     emailjs
-//       .sendForm(
-//         "service_08wafrv",
-//         "template_9y4fetl",
-//         form.current,
-//         "Tq34g58qqHkrvE_OZ"
-//       )
-//       .then(
-//         (result) => {
-//           console.log(result.text);
-//         },
-//         (error) => {
-//           console.log(error.text);
-//         }
-//       );
-//   };
-
-//   return (
-//     <div>
-//       <h4 className="text-[#FF9143] font-bold text-[32px]">
-//         Trabajemos Juntos
-//       </h4>
-//       <form
-//         ref={form}
-//         onSubmit={sendEmail}
-//         className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96"
-//       >
-//         <div className="mb-1 flex flex-col gap-6">
-//           <label variant="h6" color="blue-gray" className="-mb-3">
-//             Nombre
-//           </label>
-//           <Input
-//             type="text"
-//             name="user_name"
-//             size="lg"
-//             placeholder="Nombre"
-//             className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-//             labelProps={{
-//               className: "before:content-none after:content-none",
-//             }}
-//           />
-//           <label variant="h6" color="blue-gray" className="-mb-3">
-//             Correo
-//           </label>
-//           <Input
-//             type="email"
-//             name="user_email"
-//             size="lg"
-//             placeholder="Correo"
-//             className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-//             labelProps={{
-//               className: "before:content-none after:content-none",
-//             }}
-//           />
-
-//           <label variant="h6" color="blue-gray" className="-mb-3">
-//             Mensaje
-//           </label>
-//           <Textarea name="message" placeholder="Mensaje..." />
-//         </div>
-//         <div className="flex justify-center">
-//           <Button
-//             type="submit"
-//             value="Send"
-//             className="mt-6 bg-transparent border-2
-//             border-[#FF9143] text-[#FF9143] font-semibold hover:scale-105"
-//             Enviar
-//           >
-//             Enviar
-//           </Button>
-//         </div>
-//       </form>
-//     </div>
-//   );
-// };
-
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
 import { Input, Button, Textarea } from "@material-tailwind/react";
-import validationSchema from "./validaciones";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-// import * as Yup from "yup";
+import validaciones from "./validaciones";
 
 export const Formulario = () => {
-  // const form = useRef();
-  // const [nombre, setNombre] = useState("");
-  // const [correo, setCorreo] = useState("");
-  // const [mensaje, setMensaje] = useState("");
-
-  // const { validarNombre, validarCorreo } = validaciones();
+  const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    // if (!validarNombre(nombre)) {
-    //   // Realiza alguna acción si el nombre no es válido
-    //   console.log("Nombre no válido");
-    //   return;
-    // }
-
-    // if (!validarCorreo(correo)) {
-    //   // Realiza alguna acción si el correo no es válido
-    //   console.log("Correo no válido");
-    //   return;
-    // }
-
-    // Resto del código para enviar el correo
     emailjs
       .sendForm(
         "service_08wafrv",
         "template_9y4fetl",
-        // form.current,
-        "form",
+        form.current,
         "Tq34g58qqHkrvE_OZ"
       )
       .then(
@@ -139,7 +31,7 @@ export const Formulario = () => {
       <h4 className="text-[#FF9143] font-bold text-[32px]">
         Trabajemos Juntos
       </h4>
-      {/* <form
+      <form
         ref={form}
         onSubmit={sendEmail}
         className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96"
@@ -153,16 +45,11 @@ export const Formulario = () => {
             name="user_name"
             size="lg"
             placeholder="Nombre"
-            value={nombre}
-            onChange={(e) => setNombre(e.target.value)}
             className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
             labelProps={{
               className: "before:content-none after:content-none",
             }}
           />
-          {!validarNombre(nombre) && (
-            <p className="text-red-500">Ingresa un nombre válido.</p>
-          )}
           <label variant="h6" color="blue-gray" className="-mb-3">
             Correo
           </label>
@@ -171,65 +58,106 @@ export const Formulario = () => {
             name="user_email"
             size="lg"
             placeholder="Correo"
-            value={correo}
-            onChange={(e) => setCorreo(e.target.value)}
             className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
             labelProps={{
               className: "before:content-none after:content-none",
             }}
           />
-          {!validarCorreo(correo) && (
-            <p className="text-red-500">Ingresa un correo válido.</p>
-          )}
+
           <label variant="h6" color="blue-gray" className="-mb-3">
             Mensaje
           </label>
-          <Textarea
-            name="message"
-            placeholder="Mensaje..."
-            value={mensaje}
-            onChange={(e) => setMensaje(e.target.value)}
-          />
+          <Textarea name="message" placeholder="Mensaje..." />
         </div>
         <div className="flex justify-center">
           <Button
             type="submit"
             value="Send"
-            onClick={sendEmail}
             className="mt-6 bg-transparent border-2
             border-[#FF9143] text-[#FF9143] font-semibold hover:scale-105"
+            Enviar
           >
             Enviar
           </Button>
         </div>
-      </form> */}
-
-      <Formik
-        initialValues={{
-          nombre: "",
-          correo: "",
-          mensaje: "",
-        }}
-        onSubmit={sendEmail}
-        validationSchema={validationSchema}
-      >
-        {({ isSubmitting, errors }) => (
-          <Form>
-            <Field name="nombre" type="text" error={errors.nombre} />
-            <Field name="correo" type="email" error={errors.correo} />
-            <Field name="mensaje" type="text" error={errors.mensaje} />
-            {/* <ErrorMessage name="nombre" />
-            <ErrorMessage name="correo" />
-            <ErrorMessage name="mensaje" /> */}
-
-            <Button type="submit" value="Send" onClick={sendEmail}>
-              Enviar
-            </Button>
-          </Form>
-        )}
-      </Formik>
+      </form>
     </div>
   );
 };
 
-export default Formulario;
+// import React from "react";
+// import emailjs from "@emailjs/browser";
+// import { Button } from "@material-tailwind/react";
+// import validationSchema from "./validaciones";
+// import { Formik, Form, Field, ErrorMessage } from "formik";
+
+// export const Formulario = () => {
+//   const sendEmail = (values, { setSubmitting }) => {
+//     emailjs
+//       .sendForm(
+//         "service_08wafrv",
+//         "template_9y4fetl",
+//         values,
+//         "Tq34g58qqHkrvE_OZ"
+//       )
+//       .then(
+//         (result) => {
+//           console.log(result.text);
+//         },
+//         (error) => {
+//           console.log(error.text);
+//         }
+//       )
+//       .finally(() => {
+//         setSubmitting(false);
+//       });
+//   };
+
+//   return (
+//     <div>
+//       <h4 className="text-[#FF9143] font-bold text-[32px]">
+//         Trabajemos Juntos
+//       </h4>
+
+//       <Formik
+//         initialValues={{
+//           nombre: "",
+//           correo: "",
+//           mensaje: "",
+//         }}
+//         validationSchema={validationSchema}
+//         onSubmit={sendEmail}
+//       >
+//         {({ isSubmitting, errors, values, handleChange }) => (
+//           <Form>
+//             <Field
+//               label="Nombre"
+//               name="nombre"
+//               type="text"
+//               error={errors.nombre}
+//             />
+//             <Field
+//               label="Correo"
+//               name="correo"
+//               type="email"
+//               error={errors.correo}
+//             />
+//             <Field
+//               label="Mensaje"
+//               name="mensaje"
+//               type="text"
+//               error={errors.mensaje}
+//               as="textarea"
+//             />
+
+//             <Button type="submit" disabled={isSubmitting}>
+//               Enviar
+//             </Button>
+//           </Form>
+//         )}
+//       </Formik>
+//     </div>
+//   );
+// };
+
+// export default Formulario;
--- src/components/Formulario/validaciones.js
+++ src/components/Formulario/validaciones.js
@@ -6,44 +6,36 @@ const validationSchema = Yup.object({
     .min(4, "El nombre debe tener al menos 4 caracteres")
     .max(15, "El nombre no puede tener más de 15 caracteres"),
 
-  email: Yup.string()
+  correo: Yup.string()
     .email("Correo electrónico inválido")
     .required("Por favor, introduce tu dirección de correo electrónico."),
 
-    mensaje: Yup.string()
+  mensaje: Yup.string()
     .required("Por favor, introduce un mensaje.")
-    .min(5, "El apellido debe tener al menos 5 caracteres")
-    .max(100, "El aellido no puede tener más de 15 caracteres"),
-
+    .min(5, "El mensaje debe tener al menos 5 caracteres") // Corregido aquí
+    .max(100, "El mensaje no puede tener más de 100 caracteres"),
 });
 
 export default validationSchema;
-
-
-
-
-
-
 
 // const validaciones = () => {
 //     const patrones = {
 //       nombre: /^[a-zA-Z\s]+$/, // Acepta solo letras y espacios
 //       correo: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, // Valida un formato básico de correo electrónico
 //     };
-  
+
 //     const validarNombre = (nombre) => {
 //       return patrones.nombre.test(nombre);
 //     };
-  
+
 //     const validarCorreo = (correo) => {
 //       return patrones.correo.test(correo);
 //     };
-  
+
 //     return {
 //       validarNombre,
 //       validarCorreo,
 //     };
 //   };
-  
+
 //   export default validaciones;
-  
